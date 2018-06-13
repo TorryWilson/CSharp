7bacc8f862d27387ce3c8cdfa6c477ef7ab3a2c3
Torry Wilson <31101191+TorryWilson@users.noreply.github.com>
2018-06-13 10:52:59 -0500

    Add files via upload

---
+++ Car.cs
@@ -0,0 +1,59 @@
+﻿using SnapsLibrary;
+
+
+public class Car
+{
+    public void StartProgram()
+    {
+        SnapsEngine.DisplayImageFromUrl("https://st.hzcdn.com/simgs/1711fefa0603fedf_4-1809/industrial-novelty-signs.jpg"); // add background image of a diner sign
+        SnapsEngine.SetTitleString("Car Counter");
+        // variables that count the number of cars, vans, bikes, and trucks
+        int car = 0;
+        int van = 0;
+        int bike = 0;
+        int truck = 0;
+        
+        
+        bool button = true;
+
+        // loop that allows user to keep clicking on different transportations until user wants sum.
+        while (button == true)
+        {
+            // stores what user selects from the 5 buttons
+            string pick = SnapsEngine.SelectFrom5Buttons("Car", "Van", "Bike", "Truck", "Sum");
+            // adds one to whichever user picks
+            if (pick == "Car")
+                car = car + 1;
+            if (pick == "Van")
+                van = van + 1;
+            if (pick == "Bike")
+                bike = bike + 1;
+            if (pick == "Truck")
+                truck = truck + 1;
+            if (pick == "Sum")
+            {
+                // if user picks sum text is cleared and then program gives user the amount of times he/she clicked on each transportation.
+                SnapsEngine.ClearTextDisplay();
+                SnapsEngine.AddLineToTextDisplay("There were " + car.ToString() + " cars");
+                SnapsEngine.Delay(10);
+                SnapsEngine.AddLineToTextDisplay("There were " + van.ToString() + " vans");
+                SnapsEngine.Delay(10);
+                SnapsEngine.AddLineToTextDisplay("There were " + bike.ToString() + " bikes");
+                SnapsEngine.Delay(10);
+                SnapsEngine.AddLineToTextDisplay("There were " + truck.ToString() + " trucks");
+                SnapsEngine.Delay(10);
+                // ends the ability to select transportations.
+                button = false;
+            }
+                {
+
+                }
+            }
+        }
+       
+
+    }
+        
+
+
+
---
+++ VoiceOfPizza.cs
@@ -0,0 +1,90 @@
+﻿using SnapsLibrary;
+
+
+public class VoiceOfPizza
+{
+    public void StartProgram()
+    {
+        SnapsEngine.SetTitleString("What kind of pizza?!");
+        // variables that count pizza types
+        int pepperoni = 0;
+        int cheese = 0;
+        int beef = 0;
+        int supreme = 0;
+        // counter variable for loop
+        int counter = 1;
+
+        // loop around 4 times unless overview is clicked
+        while (counter <= 4)
+        {
+
+            {
+                // asks user to say favorite pizza or overview 
+                string toppingChoice = SnapsEngine.SelectFromFiveSpokenPhrases(prompt: "What's your favorite pizza?", phrase1: "Pepperoni", phrase2: "Cheese", phrase3: "Supreme", phrase4: "Beef", phrase5: "Overview");
+                // adds one to whatever pizza user chose
+                if (toppingChoice == "Pepperoni")
+                    pepperoni = pepperoni + 1;
+                if (toppingChoice == "Cheese")
+                    cheese = cheese + 1;
+                if (toppingChoice == "Beef")
+                    beef = beef + 1;
+                if (toppingChoice == "Supreme")
+                    supreme = supreme + 1;
+
+                // if user selects overview, it will display and say what choices he/she chose
+                if (toppingChoice == "Overview")
+                {
+                    SnapsEngine.ClearTextDisplay();
+                    SnapsEngine.SpeakString("There were " + pepperoni + " pepperoni pizzas");
+                    SnapsEngine.AddLineToTextDisplay("There were " + pepperoni + " pepperoni pizzas");
+                    SnapsEngine.Delay(3);
+                    SnapsEngine.SpeakString("There were " + cheese + " cheese pizzas");
+                    SnapsEngine.AddLineToTextDisplay("There were " + cheese + " cheese pizzas");
+                    SnapsEngine.Delay(3);
+                    SnapsEngine.SpeakString("There were " + beef + " beef pizzas");
+                    SnapsEngine.AddLineToTextDisplay("There were " + beef + " beef pizzas");
+                    SnapsEngine.Delay(3);
+                    SnapsEngine.SpeakString("There were " + supreme + " supreme pizzas");
+                    SnapsEngine.AddLineToTextDisplay("There were " + supreme + " supreme pizzas");
+                    SnapsEngine.Delay(3);
+                    // exits loop
+                    break;
+
+                }
+                // if no logical selection has been chosen, alerts users of error and repeats the prompt
+                else
+                {
+                    SnapsEngine.SpeakString("Sorry that is not a viable selection!");
+                    continue;
+                }
+
+           
+                counter = counter + 1;
+            }
+        }    
+       // if user says 4 pizza choices, this block runs. its an overview without clicking on it after max 4 choices
+            SnapsEngine.ClearTextDisplay();
+            SnapsEngine.SpeakString("There were " + pepperoni + " pepperoni pizzas");
+            SnapsEngine.AddLineToTextDisplay("There were " + pepperoni + " pepperoni pizzas");
+            SnapsEngine.Delay(3);
+            SnapsEngine.SpeakString("There were " + cheese + " cheese pizzas");
+            SnapsEngine.AddLineToTextDisplay("There were " + cheese + " cheese pizzas");
+            SnapsEngine.Delay(3);
+            SnapsEngine.SpeakString("There were " + beef + " beef pizzas");
+            SnapsEngine.AddLineToTextDisplay("There were " + beef + " beef pizzas");
+            SnapsEngine.Delay(3);
+            SnapsEngine.SpeakString("There were " + supreme + " supreme pizzas");
+            SnapsEngine.AddLineToTextDisplay("There were " + supreme + " supreme pizzas");
+            SnapsEngine.Delay(3);
+
+
+
+        }
+
+        
+
+    }
+        
+
+
+
